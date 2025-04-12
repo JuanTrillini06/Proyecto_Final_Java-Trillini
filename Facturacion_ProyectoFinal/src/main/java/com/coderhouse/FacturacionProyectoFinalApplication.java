2541dc9c67e9e6cf585a64a532cd4170c38afc79
JuanTrillini06 <juanjose_207@hotmail.com>
2025-04-12 09:15:14 -0300

    Creacion de la base del proyecto

---
+++ Facturacion_ProyectoFinal/src/main/java/com/coderhouse/FacturacionProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionProyectoFinalApplication.class, args);
+	}
+
+}
